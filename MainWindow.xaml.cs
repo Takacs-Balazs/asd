05b2a640faf7df186c1bf99826bd341030f9b651
Takacs-Balazs <71711153+Takacs-Balazs@users.noreply.github.com>
2023-03-17 18:52:29 +0100

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,129 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfOsztalyzas
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string fajlNev = "naplo.txt";
+        //Így minden metódus fogja tudni használni.
+        ObservableCollection<Osztalyzat> jegyek = new ObservableCollection<Osztalyzat>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            // Ha nem választ ki semmit, akkor "naplo.csv" legyen az állomány neve. A későbbiekben ebbe fog rögzíteni a program.
+            dgJegyek.ItemsSource = jegyek;
+
+            OpenFileDialog ofd = new OpenFileDialog();
+
+
+            if (ofd.ShowDialog() == true)
+            {
+                fajlNev = ofd.FileName;
+            }
+            else
+            {
+                fajlNev = "naplo.txt";
+            }
+
+            Betolt();
+            Atlag();
+        }
+
+        private void btnRogzit_Click(object sender, RoutedEventArgs e)
+        {
+            // a) - A név legalább két szóból álljon és szavanként minimum 3 karakterből!
+            //      Szó = A szöközökkel határolt karaktersorozat.
+            // b) - A beírt dátum újabb, mint a mai dátum
+
+
+
+            //A CSV szerkezetű fájlba kerülő sor előállítása
+            string csvSor = $"{txtNev.Text};{datDatum.Text};{cboTantargy.Text};{sliJegy.Value}";
+            //Megnyitás hozzáfűzéses írása (APPEND)
+            StreamWriter sw = new StreamWriter(fajlNev, append: true);
+
+            if (csvSor.Split(";")[0].Split(" ").Length >=2 && csvSor.Split(";")[0].Split(" ")[0].Length>=3 && csvSor.Split(";")[0].Split(" ")[1].Length >=3)
+            {
+                sw.WriteLine(csvSor);
+            }
+            else
+            {
+                MessageBox.Show("Két tagu nevet adj meg.");
+            }
+            
+            Osztalyzat ujJegy = new Osztalyzat(txtNev.Text, datDatum.Text, cboTantargy.Text, Convert.ToInt32(sliJegy.Value));
+            jegyek.Add(ujJegy);
+            Atlag();
+            sw.Close();
+            dgJegyek.ItemsSource = jegyek;
+        }
+
+        private void btnBetolt_Click(object sender, RoutedEventArgs e)
+        {
+            jegyek.Clear();  //A lista előző tartalmát töröljük
+            Betolt(); //állomány lezárása
+
+            //A Datagrid adatforrása a jegyek nevű lista lesz.
+            //A lista objektumokat tartalmaz. Az objektumok lesznek a rács sorai.
+            //Az objektum nyilvános tulajdonságai kerülnek be az oszlopokba.
+            dgJegyek.ItemsSource = jegyek;
+            Atlag();
+        }
+
+        private void Betolt()
+        {
+            StreamReader sr = new StreamReader(fajlNev); //olvasásra nyitja az állományt
+            while (!sr.EndOfStream) //amíg nem ér a fájl végére
+            {
+                string[] mezok = sr.ReadLine().Split(";"); //A beolvasott sort feltördeli mezőkre
+                //A mezők értékeit felhasználva létrehoz egy objektumot
+                Osztalyzat ujJegy = new Osztalyzat(mezok[0], mezok[1], mezok[2], int.Parse(mezok[3]));
+                jegyek.Add(ujJegy); //Az objektumot a lista végére helyezi
+            }
+            lblfilename.Content = fajlNev;
+            Atlag();
+            sr.Close();
+        }
+
+        private void sliJegy_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            lblJegy.Content = sliJegy.Value; //Több alternatíva van e helyett! Legjobb a Data Binding!
+        }
+
+
+
+        //todo Helyezzen el alkalmas helyre 2 rádiónyomógombot!
+        //Feliratok: [■] Vezetéknév->Keresztnév [O] Keresztnév->Vezetéknév
+        //A táblázatban a név azserint szerepeljen, amit a rádiónyomógomb mutat!
+        //A feladat megoldásához használja fel a ForditottNev metódust!
+        //Módosíthatja az osztályban a Nev property hozzáférhetőségét!
+        //Megjegyzés: Felételezzük, hogy csak 2 tagú nevek vannak
+    public void Atlag()
+    {
+        lblatlag.Content = Math.Round(jegyek.Average(x => x.Jegy),2);
+    }
+    }
+
+
+}
+
---
+++ Osztalyzat.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfOsztalyzas
+{
+    public class Osztalyzat
+    {
+        String nev;
+        String datum;
+        String tantargy;
+        int jegy;
+        string CsaladiNev;
+
+        public Osztalyzat(string csaladiNev)
+        {
+            CsaladiNev = csaladiNev;
+        }
+
+        public Osztalyzat(string nev, string datum, string tantargy, int jegy)
+        {
+            this.nev = nev;
+            this.datum = datum;
+            this.tantargy = tantargy;
+            this.jegy = jegy;
+        }
+
+        public string Nev { get => nev;  }
+        public string Datum { get => datum;  }
+        public string Tantargy { get => tantargy; }
+        public int Jegy { get => jegy; }
+        public string CsaladiNev1 { get => nev.Split(" ")[0]; }
+        public void ForditottNev()
+        {
+          nev = nev.Split(" ")[1] + " " + nev.Split(" ")[0];
+        }
+    }
+
+
+        
+}
